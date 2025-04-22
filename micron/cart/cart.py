16eae9a1d111a59a85447533b61c1f9742234b6c
sauberr <sauberr10@gmail.com>
2025-04-22 20:41:57 +0300

    Feat: Delete coupon id from session.

--- micron/cart/cart.py
+++ micron/cart/cart.py
@@ -119,4 +119,6 @@ class Cart:
     def clear(self):
         # Remove all from session
         del self.session[settings.CART_SESSION_ID]
+        if self.coupon_id:
+            del self.session["coupon_id"]
         self.save()
